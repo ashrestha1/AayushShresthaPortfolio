76dd4faea40f809c13635199a4c341478b6754df
ashrestha1 <oscaaah@gmail.com>
2021-02-13 03:58:14 +0800

    portfolio draft

--- src/components/my-navbar/my-navbar.component.jsx
+++ src/components/my-navbar/my-navbar.component.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Logo from "../../assets/icons/logow.webp";
-import "./my-navbar.styles.css";
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './my-navbar.styles.css';
 
 const MyNavbar = () => {
   return (
@@ -11,18 +10,15 @@ const MyNavbar = () => {
         fixed="top"
         variant="dark"
         expand="md"
-        className="animate-navbar nav-theme justify-content-between"
+        className="animate-navbar nav-theme bar"
       >
         <div>
-          <Navbar.Brand href="#home">
-            <img className="logo" src={Logo} alt="" />
-          </Navbar.Brand>
+          <span className="name">Aayush Shrestha</span>
         </div>
         <div>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto ">
-              <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#about">About</Nav.Link>
               <Nav.Link href="#skills">Skills</Nav.Link>
               <Nav.Link href="#experience">Experience</Nav.Link>
--- src/pages/contact-form/contact-form.component.jsx
+++ src/pages/contact-form/contact-form.component.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import "./contact-form.styles.css";
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import './contact-form.styles.css';
 
 const ContactForm = () => {
   return (
@@ -13,45 +13,28 @@ const ContactForm = () => {
         <Row>
           <Col className="d-flex justify-content-center flex-wrap">
             <div className="m-2">
-              <a href="mailto:akjha96@gmail.com" target="_blank" rel="noopener noreferrer">
-                <Button variant="outline-danger" title="akjha96@gmail.com">
+              <a
+                href="mailto:aayush.shrestha.hk@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button
+                  variant="outline-danger"
+                  title="aayush.shrestha.hk@gmail.com"
+                >
                   <i className="fas fa-envelope-square"></i> Email Me
                 </Button>
               </a>
             </div>
+
             <div className="m-2">
-              <a href="https://www.linkedin.com/in/anand-kumar-jha-745798a4/" target="_blank" rel="noopener noreferrer">
-                <Button variant="outline-primary" title="Visit my LinkenIn">
-                  <i className="fab fa-linkedin"></i> LinkedIn
-                </Button>
-              </a>
-            </div>
-            
-            <div className="m-2">
-              <a href="https://github.com/akjha96" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://github.com/ashrestha1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <Button variant="outline-dark" title="My other projects">
                   <i className="fab fa-github-square"></i> GitHub
-                </Button>
-              </a>
-            </div>
-            <div className="m-2">
-              <a href="https://www.youtube.com/user/MrAkjha96/" target="_blank" rel="noopener noreferrer">
-                <Button id="youtube-btn" variant="outline-info" title="Lets code together!">
-                <i class="fab fa-youtube"></i> Youtube
-                </Button>
-              </a>
-            </div>
-            <div className="m-2">
-              <a href="https://twitter.com/akjha96" target="_blank" rel="noopener noreferrer">
-                <Button variant="outline-info" title="Tweets are welcomed!">
-                  <i className="fab fa-twitter"></i> Twitter
-                </Button>
-              </a>
-            </div>
-            <div className="m-2">
-              <a href="https://www.facebook.com/akjha132" target="_blank" rel="noopener noreferrer">
-                <Button variant="outline-primary" title="Say hello on FB">
-                  <i className="fab fa-facebook-square"></i> FaceBook
                 </Button>
               </a>
             </div>
